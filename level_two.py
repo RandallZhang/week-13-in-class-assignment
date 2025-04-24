62a51c13f560fdce54710f63c0ec347ee07d742c
apple <apple@Randalls-MacBook-Pro.local>
2025-04-24 09:08:00 -0400

    level 2 complete

---
+++ level_two.py
@@ -0,0 +1,6 @@
+Laptop_inventory = ["desktop", 42, 28.28, False]
+
+playlist_for_coding = ["I wanna dance with somebody by whiteny Houston", "Slow dance in a burning room by John Mayer"
+                       "Just the way you are by Bruno Mars", "drivers License by Olivia Rodrigo"
+                       "I didn't mean to crash your car by Randall Zhang"]
+playlist_for_coding.append("Try by P!nk")
